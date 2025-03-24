6b19f2e241c85c173487331153637d423b1df73a
Pedro Avilar <pedrowa.avilar@gmail.com>
2025-03-24 20:14:46 -0300

    Estilização para title e input.

--- styles/styles.ts
+++ styles/styles.ts
@@ -7,5 +7,17 @@ export const styles = StyleSheet.create({
         backgroundColor: '#dff',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        margin: 15,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        borderRadius: 5,
+        marginBottom: 10,
+    },
 })
